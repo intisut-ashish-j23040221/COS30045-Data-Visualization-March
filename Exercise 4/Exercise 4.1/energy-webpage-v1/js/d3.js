2b9bbedf2edfd2275575f4b9fbe237f8130a7c28
J23040221 - Ashish Emmanuel <106224123@student.swin.edu.au>
2026-05-21 16:53:52 +0800

    Added new SVG element using D3

--- Exercise 4/Exercise 4.1/energy-webpage-v1/js/d3.js
+++ Exercise 4/Exercise 4.1/energy-webpage-v1/js/d3.js
@@ -1,2 +1,4 @@
 d3.select("h2").style("color", "red");
 d3.select("div.flex").append("h3").text("Energy consumption is something that all TV manufacters should take into consideration.");
+
+d3.select("svg").append("rect").attr("x", 30).attr("y", 50).attr("width", 100).attr("height", 30).style("fill", "red");
