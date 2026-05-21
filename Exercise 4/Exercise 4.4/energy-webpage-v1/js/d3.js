7a4f0c18ac95dd47f185ef36c78b85b64a65de55
J23040221 - Ashish Emmanuel <106224123@student.swin.edu.au>
2026-05-21 21:13:06 +0800

    Load CSV Data using D3

--- Exercise 4/Exercise 4.4/energy-webpage-v1/js/d3.js
+++ Exercise 4/Exercise 4.4/energy-webpage-v1/js/d3.js
@@ -1,2 +1,12 @@
+// Exercise 4.3
+
 const svg = d3.select("#e2 .responsive-svg-container").append("svg").attr("viewBox", "0 0 1200 1600").style("border", "1px solid black");
 svg.append("rect").attr("x", 10).attr("y", 10).attr("width", 414).attr("height", 16).attr("fill", "blue");
+
+
+
+
+// Exercise 4.4
+d3.csv("../data/newDataTV.csv", d => {
+  console.log(d); 
+});
