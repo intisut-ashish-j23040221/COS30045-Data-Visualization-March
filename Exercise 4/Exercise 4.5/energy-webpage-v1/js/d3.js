fea2b1578f37cba55e0f71b7151cf4f8626c38d6
J23040221 - Ashish Emmanuel <106224123@student.swin.edu.au>
2026-05-21 21:40:37 +0800

    Loading the SVG elements for data

--- Exercise 4/Exercise 4.5/energy-webpage-v1/js/d3.js
+++ Exercise 4/Exercise 4.5/energy-webpage-v1/js/d3.js
@@ -4,10 +4,8 @@ const svg = d3.select("#e2 .responsive-svg-container").append("svg").attr("viewB
 svg.append("rect").attr("x", 10).attr("y", 10).attr("width", 414).attr("height", 16).attr("fill", "blue");
 
 
-
-
 // Exercise 4.4
-const createBarChart = (data) => {
+const logData = (data) => {
     console.log(data);
     console.log(data.length);
     console.log(d3.max(data, d => d.count));
@@ -21,4 +19,16 @@ const createBarChart = (data) => {
     }
 }
 
-d3.csv("./data/newDataTV.csv", d => ({ brand: d.Brand_Reg, count: +d["Count(Model_No)"] })).then(createBarChart);
+d3.csv("./data/newDataTV.csv", d => ({ brand: d.Brand_Reg, count: +d["Count(Model_No)"] })).then(logData);
+
+
+// Exercise 4.5
+const createBarChart = data => {
+    const svg = d3.select("#e4 .responsive-svg-container").append("svg").attr("viewBox", "0 0 1200 1600").style("border", "1px solid black");
+    svg.selectAll("rect").data(data).join("rect").attr("class", d => {
+        console.log(d);
+        return `bar bar-${d.count}`;
+    });   
+}
+
+d3.csv("./data/newDataTV.csv", d => ({ brand: d.Brand_Reg, count: +d["Count(Model_No)"] })).then(d => createBarChart(d.sort((a, b) => b.count - a.count)));
