4cf06c0b51fc278cdfd9248f68879589025ba8a8
J23040221 - Ashish Emmanuel <106224123@student.swin.edu.au>
2026-05-21 16:51:14 +0800

    Appending element with d3

--- Exercise 4/Exercise 4.1/energy-webpage-v1/js/d3.js
+++ Exercise 4/Exercise 4.1/energy-webpage-v1/js/d3.js
@@ -1,4 +1,2 @@
-// Will be used later
-
-d3.select("h2")
-  .style("color", "red")
+d3.select("h2").style("color", "red");
+d3.select("div.flex").append("p").text("Energy consumption is something that all TV manufacters should take into consideration.");
