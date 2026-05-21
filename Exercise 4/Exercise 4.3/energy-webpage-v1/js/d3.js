1801374a503b2cd3540b31f659052f86c2ae897e
J23040221 - Ashish Emmanuel <106224123@student.swin.edu.au>
2026-05-21 20:38:09 +0800

    Add new SVG container

--- Exercise 4/Exercise 4.3/energy-webpage-v1/js/d3.js
+++ Exercise 4/Exercise 4.3/energy-webpage-v1/js/d3.js
@@ -1,4 +1,4 @@
-d3.select("h2").style("color", "red");
-d3.select("div.flex").append("h3").text("Energy consumption is something that all TV manufacters should take into consideration.");
-
-d3.select("svg").append("rect").attr("x", 30).attr("y", 50).attr("width", 100).attr("height", 30).style("fill", "red");
+const svg = d3.select(".responsive-svg-container")
+    .append("svg")
+      .attr("viewBox", "0 0 1200 1600")
+      .style("border", "1px solid black");
