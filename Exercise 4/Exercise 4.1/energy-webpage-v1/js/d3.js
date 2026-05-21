5e72019c8d5c15afd21bfeecd0306b359332be64
J23040221 - Ashish Emmanuel <106224123@student.swin.edu.au>
2026-05-21 16:51:46 +0800

    Change p element in d3 to h3

--- Exercise 4/Exercise 4.1/energy-webpage-v1/js/d3.js
+++ Exercise 4/Exercise 4.1/energy-webpage-v1/js/d3.js
@@ -1,2 +1,2 @@
 d3.select("h2").style("color", "red");
-d3.select("div.flex").append("p").text("Energy consumption is something that all TV manufacters should take into consideration.");
+d3.select("div.flex").append("h3").text("Energy consumption is something that all TV manufacters should take into consideration.");
